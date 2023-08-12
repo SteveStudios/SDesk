39189c2fd4eaa3a64b2f5e76b5f1dcb745f4bfba
Stephen <magnumsrtrblx@gmail.com>
2023-08-12 14:02:28 -0400

    Add the default background to the dark theme

--- airootfs/etc/profile.d/setup-final.sh
+++ airootfs/etc/profile.d/setup-final.sh
@@ -4,6 +4,8 @@ if (( $EUID != 0 )) && [ ! -f "/etc/profile.d/install.sh" ] && [ ! -f "/etc/prof
 	dconf reset -f /org/gnome/
 	
 	gsettings set org.gnome.desktop.background picture-uri file:////usr/share/pixmaps/sd-bg-default.png
+	gsettings set org.gnome.desktop.background picture-uri-dark file:////usr/share/pixmaps/sd-bg-default.png
+
  	gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Music.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop']"
 		
 	xdg-settings set default-web-browser firefox.desktop
