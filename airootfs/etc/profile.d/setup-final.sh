ee10b02e0af219169514dd844e850f7bb603c9ff
Stephen Byrne <magnumsrtrblx@gmail.com>
2023-09-08 18:55:07 -0400

    iso 4

--- airootfs/etc/profile.d/setup-final.sh
+++ airootfs/etc/profile.d/setup-final.sh
@@ -17,9 +17,9 @@ if (( $EUID != 0 )) && [ ! -f "/etc/profile.d/install.sh" ] && [ ! -f "/etc/prof
 	gsettings set org.gnome.desktop.background picture-uri file:////usr/share/pixmaps/sd-bg-default.png
 	gsettings set org.gnome.desktop.background picture-uri-dark file:////usr/share/pixmaps/sd-bg-default.png
 
- 	gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Music.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop']"
+ 	gsettings set org.gnome.shell favorite-apps "['swirl.desktop', 'org.gnome.Geary.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Music.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop']"
 		
-	xdg-settings set default-web-browser firefox.desktop
+	xdg-settings set default-web-browser swirl.desktop
 
 	rm -f /etc/profile.d/setup-final.sh
 	chmod -R 700 /etc/profile.d
--- airootfs/etc/profile.d/setup.sh
+++ airootfs/etc/profile.d/setup.sh
@@ -23,9 +23,9 @@ if (( $EUID != 0 )); then
 	gsettings set org.gnome.desktop.background picture-uri file:////usr/share/pixmaps/sd-bg-default.png
 	gsettings set org.gnome.desktop.background picture-uri-dark file:////usr/share/pixmaps/sd-bg-default.png
 	
- 	gsettings set org.gnome.shell favorite-apps "['calamares.desktop', 'firefox.desktop', 'thunderbird.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Music.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop']"
-		
-	xdg-settings set default-web-browser firefox.desktop
+ 	gsettings set org.gnome.shell favorite-apps "['calamares.desktop', 'swirl.desktop', 'org.gnome.Geary.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Music.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop']"
+
+	xdg-settings set default-web-browser swirl.desktop
 	
 	sudo systemctl enable bluetooth
 	sudo systemctl start bluetooth
