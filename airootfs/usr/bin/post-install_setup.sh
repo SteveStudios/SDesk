fe1b491250acc6179f640a5e5dc7bb2436a89361
Stephen Byrne <stevestudiosbusiness@gmail.com>
2023-09-24 11:25:44 -0400

    fix useless file

--- airootfs/usr/bin/post-install_setup.sh
+++ airootfs/usr/bin/post-install_setup.sh
@@ -3,8 +3,8 @@
 dconf reset -f /org/gnome/
 	
 gsettings set org.gnome.desktop.background picture-uri file:////usr/share/pixmaps/sd-bg-default.png
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'thunderbird.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Music.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop']"
+gsettings set org.gnome.shell favorite-apps "['swirl.desktop', 'org.gnome.Geary.desktop', 'org.gnome.Calendar.desktop', 'org.gnome.Music.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Software.desktop']"
 		
-xdg-settings set default-web-browser firefox.desktop
+xdg-settings set default-web-browser swirl.desktop
 	
 sudo rm -rf usr/bin/post-install_setup.sh
