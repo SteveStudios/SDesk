bc65803e6433aeeaba995c5267cdc13f21d8b0f4
Stephen Byrne <stevestudiosbusiness@gmail.com>
2024-09-15 08:09:18 -0400

    ...

--- airootfs/etc/profile.d/setup-post.sh
+++ airootfs/etc/profile.d/setup-post.sh
@@ -2,9 +2,10 @@
 
 if (( EUID != 0 )) && [ ! -f "/etc/profile.d/install.sh" ] && [ ! -f "/etc/profile.d/setup.sh" ] && [ ! -f "/usr/bin/setup-final.sh" ]; then
 	sudo rm -rf /etc/profile.d/setup-post.sh
+	sudo rm -rf /var/lib/AccountsService/users/live
+
 	sudo userdel -f live
 
-	sudo mv /etc/gdm/lastcustom.conf /etc/gdm/custom.conf
 	sudo mv /etc/tmpsudoers /etc/sudoers
 
 	gsettings set org.gnome.shell welcome-dialog-last-shown-version '4294967295'
